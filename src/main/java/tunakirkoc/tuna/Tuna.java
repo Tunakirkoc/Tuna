6522f186c837c0a8e3357d86e49d66f293d4b811
Tuna Kirkoc <tuna@kirkoc.fr>
2022-02-19 18:00:33 +0100

    Add Block

--- src/main/java/tunakirkoc/tuna/Tuna.java
+++ src/main/java/tunakirkoc/tuna/Tuna.java
@@ -1,8 +1,13 @@
 package tunakirkoc.tuna;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -17,6 +22,7 @@ public class Tuna implements ModInitializer {
 
 	// instance of the item
 	public static final Item GRANT = new Item(new Item.Settings().group(ItemGroup.MISC));
+	public static final Block GRANT_BLOCK = new Block(FabricBlockSettings.of(Material.DECORATION).strength(5.0F, 5.0F).sounds(BlockSoundGroup.BONE).breakInstantly());
 
 	@Override
 	public void onInitialize() {
@@ -28,5 +34,7 @@ public class Tuna implements ModInitializer {
 
 		// Init the item
 		Registry.register(Registry.ITEM, new Identifier("tuna", "grant"), GRANT);
+		Registry.register(Registry.BLOCK, new Identifier("tuna", "grant_block"), GRANT_BLOCK);
+		Registry.register(Registry.ITEM, new Identifier("tuna", "grant_block"), new BlockItem(GRANT_BLOCK, new Item.Settings().group(ItemGroup.MISC)));
 	}
 }
